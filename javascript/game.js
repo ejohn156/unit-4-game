fd9e6b54516823d6ad275dfec25312dfb127d10d
Eric Johnson <ejohn156@uncc.edu>
2018-06-06 13:17:09 -0400

    completed game

--- javascript/game.js
+++ javascript/game.js
@@ -1,60 +1,131 @@
-var targetNumber = 53;
+$(document).ready(function () {
+    var desiredScore;
+    var crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+    var currentScore = 0;
+    var wins = 0;
+    var losses = 0;
+    desiredScore = Math.floor(Math.random() * 101) + 19
+    $("#desiredscore").text(desiredScore)
+    $("#currentscore").text(currentScore)
+    $("#wins").text(wins)
+    $("#losses").text(currentScore)
 
-$("#number-to-guess").text(targetNumber);
 
-var counter = 0;
+    $("#img1").on("click", function () {
+        currentScore += crystalPoints[0]
+        $("#currentscore").text(currentScore)
 
-// Now for the hard part. Creating multiple crystals each with their own unique number value.
+        if (currentScore == desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            wins++
+           
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(losses)
 
-// We begin by expanding our array to include four options.
-var numberOptions = [10, 5, 3, 7];
-var imgArray = ["../images/crystalimg1.jpg", "../images/crystalimg2.jpg", "../images/crystalimg3.jpg", "../images/crystalimg4.jpg"]
+        }
+        else if (currentScore > desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            losses++
+            
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(losses)
 
-// Next we create a for loop to create crystals for every numberOption.
-for (var i = 0; i < numberOptions.length; i++) {
+        }
+    });
+    $("#img2").on("click", function () {
+        currentScore += crystalPoints[1]
+        $("#currentscore").text(currentScore)
 
-  // For each iteration, we will create an imageCrystal
-  var imageCrystal = $("<img>");
-  
+        if (currentScore == desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            wins++
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(losses)
+        }
+        else if (currentScore > desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            losses++
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(losses)
+        }
+    });
+    $("#img3").on("click", function () {
+        currentScore += crystalPoints[2]
+        $("#currentscore").text(currentScore)
 
-  // First each crystal will be given the class ".crystal-image".
-  // This will allow the CSS to take effect.
-  imageCrystal.addClass("crystal-image");
+        if (currentScore == desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            wins++
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(losses)
+        }
+        else if (currentScore > desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            losses++
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(losses)
+        }
+    });
+    $("#img4").on("click", function () {
+        currentScore += crystalPoints[3]
+        $("#currentscore").text(currentScore)
 
-  // Each imageCrystal will be given a src link to the crystal image
-  imageCrystal.attr("src", imgArray[i]);
+        if (currentScore == desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            wins++
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(currentScore)
+        }
+        else if (currentScore > desiredScore) {
+            
+            desiredScore = Math.floor(Math.random() * 101) + 19
+            crystalPoints = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1]
+            currentScore = 0;
+            losses++
+            $("#desiredscore").text(desiredScore)
+            $("#currentscore").text(currentScore)
+            $("#wins").text(wins)
+            $("#losses").text(currentScore)
+        }
+    });
 
-  // Each imageCrystal will be given a data attribute called data-crystalValue.
-  // This data attribute will be set equal to the array value.
-  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
-  // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-  $("#crystals").append(imageCrystal);
-}
 
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function() {
-
-  // Determining the crystal's value requires us to extract the value from the data attribute.
-  // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-  // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-  // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-  var crystalValue = ($(this).attr("data-crystalvalue"));
-  crystalValue = parseInt(crystalValue);
-  // We then add the crystalValue to the user's "counter" which is a global variable.
-  // Every click, from every crystal adds to the global counter.
-  counter += crystalValue;
-
-  // All of the same game win-lose logic applies. So the rest remains unchanged.
-  alert("New score: " + counter);
-
-  if (counter === targetNumber) {
-    alert("You win!");
-  }
-
-  else if (counter >= targetNumber) {
-    alert("You lose!!");
-  }
 
 });
+
